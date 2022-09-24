d1c99e20ec034623ff6eaf6a3a1559e603a2471a
glokta1 <rafeyahmad@protonmail.com>
2022-09-25 01:29:56 +0530

    Remove extraneous code and comments

--- hashing.c
+++ hashing.c
@@ -13,11 +13,6 @@ void search(int key);
 void delete(int key);
 void display();
 int generate_address(int option, int key, int i);
-// void linear_probing();
-void quad_probing();
-void double_hashing();
-void sep_chaining();
-void handle_collisions(int option, int key, int address);
 
 int main (void) {
     printf("Welcome!\n1) Linear probing\n2) Quadratic probing\n3) Double Hashing\n4) Separate Chaining\n");
@@ -66,23 +61,6 @@ int generate_address(int option, int key, int i) {
     }
 }
 
-// void handle_collisions(int option, int key, int address) {
-//     switch (option) {
-//         case 1:
-//             linear_probing(key, address);
-//             break;
-//         // case 2:
-//         //     quad_probing(key, address);
-//         //     break;
-//         // case 3: 
-//         //     double_hashing(key, address);
-//         //     break;
-//         // case 4:
-//         //     sep_chaining(key, address);
-//         //     break;
-//     }
-// }
-
 int hash(int key) {
     return key % TABLE_SIZE;
 }
@@ -97,13 +75,6 @@ void insert(int key) {
         }
     }
 }
-
-// void linear_probing(int key, int address) {
-//     for (int i = 1; i < TABLE_SIZE; i++) {
-//         address = (hash(key) + i) % TABLE_SIZE;
-
-//     }
-// }
 
 void search(int key) {
     int found = 0;
