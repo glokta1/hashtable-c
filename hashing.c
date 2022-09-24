b50d84236c6c92bd0a19bc9b9d423751206b6ee4
glokta1 <rafeyahmad@protonmail.com>
2022-09-25 01:06:34 +0530

    add separate function for generating addresses of keys. Will make code reusable for search and delete operations

--- hashing.c
+++ hashing.c
@@ -12,7 +12,8 @@ void insert(int key);
 void search(int key);
 void delete(int key);
 void display();
-void linear_probing();
+int generate_address(int option, int key, int i);
+// void linear_probing();
 void quad_probing();
 void double_hashing();
 void sep_chaining();
@@ -20,7 +21,7 @@ void handle_collisions(int option, int key, int address);
 
 int main (void) {
     printf("Welcome!\n1) Linear probing\n2) Quadratic probing\n3) Double Hashing\n4) Separate Chaining\n");
-    printf("\nSelect a collision resolution method: ");
+    printf("\nSelect a collision resolution method [1-4]: ");
     scanf("%d", &option);
 
     // initializing arrays with -1 to indicate unoccupied location
@@ -30,9 +31,9 @@ int main (void) {
 
     // initialize random number generator
     insert(5);
-    insert(8);
-    insert(6);
-    insert(7);
+    insert(10);
+    insert(15);
+    insert(9);
     display();
 
 }
@@ -47,49 +48,55 @@ void display() {
     }
 }
 
-void handle_collisions(int option, int key, int address) {
+int generate_address(int option, int key, int i) {
     switch (option) {
         case 1:
-            linear_probing(key, address);
+            return (hash(key)  + i) % TABLE_SIZE;
             break;
-        // case 2:
-        //     quad_probing(key, address);
-        //     break;
-        // case 3: 
-        //     double_hashing(key, address);
-        //     break;
-        // case 4:
-        //     sep_chaining(key, address);
-        //     break;
+        case 2:
+            return (hash(key) + i*i) % TABLE_SIZE;
+            break;
     }
 }
+
+// void handle_collisions(int option, int key, int address) {
+//     switch (option) {
+//         case 1:
+//             linear_probing(key, address);
+//             break;
+//         // case 2:
+//         //     quad_probing(key, address);
+//         //     break;
+//         // case 3: 
+//         //     double_hashing(key, address);
+//         //     break;
+//         // case 4:
+//         //     sep_chaining(key, address);
+//         //     break;
+//     }
+// }
 
 int hash(int key) {
     return key % TABLE_SIZE;
 }
 
 void insert(int key) {
-    int address = hash(key);
-    if (ht[address] == -1) {
-        ht[address] = key;
-    } else {
-        handle_collisions(1, key, address);
-    }
-
-}
-
-// void insert(int key) {
-// }
-
-void linear_probing(int key, int address) {
     for (int i = 0; i < TABLE_SIZE; i++) {
-        address = (hash(key) + i) % TABLE_SIZE;
+        int address = generate_address(option, key, i);
+
         if (ht[address] == -1) {
             ht[address] = key;
-            break;
+            return;
         }
     }
 }
+
+// void linear_probing(int key, int address) {
+//     for (int i = 1; i < TABLE_SIZE; i++) {
+//         address = (hash(key) + i) % TABLE_SIZE;
+
+//     }
+// }
 
 void search(int key) {
 }
